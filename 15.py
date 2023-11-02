9641610134289830ae927ac0b472257405d87290
abdrrman <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-11-02 08:44:27 +0000

    req

--- 15.py
+++ 15.py
@@ -1,3 +1,51 @@
 
+
+import os
 import streamlit as st
-st.title('app-4-15')
+import tempfile
+imports
+
+
+function_defs
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+openai_api_key = st.sidebar.text_input(
+    "OpenAI API Key",
+    placeholder="sk-...",
+    value=os.getenv("OPENAI_API_KEY", ""),
+    type="password",
+)
+
+
+st.sidebar.markdown("""'''
+# How to use
+
+1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) above🔑
+2. Navigate to the sidebar and input your OpenAI API Key in the "OpenAI API Key" field. The placeholder is "sk-...". This field is password protected for your security.
+3. In the main application window, find the "Enter blog content" text area. Here, you can input the content of your blog post.
+4. Below the blog content field, you'll find another text input field labeled "Enter markdown formatting". Here, you can input the markdown formatting you want to apply to your blog content.
+5. After entering your blog content and markdown formatting, the application will automatically generate a formatted blog post. If you don't provide any markdown formatting, the blog content will be returned as is.
+6. The generated blog post will be displayed in the application window. If no blog content or markdown formatting is provided, an empty string will be displayed.
+'''""")
+
+
+st.sidebar.markdown("# About")
+st.sidebar.markdown("Dark AI Diaries is a sleek, dark-mode blog website that leverages the latest trends in AI and Python. It features a robust Content Management System (CMS) and supports markdown formatting for easy content creation and management.")
+
+with st.form(key="form"):
+    st.title('Dark AI Diaries')
+    blog_content = st.text_area("Enter blog content")
+    markdown_formatting = st.text_input("Enter markdown formatting")
+
+    submit_button = st.form_submit_button(label='Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submit_button:
+
+        remaining_code
+        if formatted_blog_post is not None and len(str(formatted_blog_post)) > 0:
+            st.markdown(formatted_blog_post)
