3bdc378a695c8bd2cbf5602ddcddc5781973487e
abdrrman <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-11-03 23:48:36 +0000

    13.py

--- 13.py
+++ 13.py
@@ -69,7 +69,7 @@ def query_google(faq):
 
 
 def summarize_doc(google_doc):
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k", openai_api_key=openai_api_key)
     chain = load_summarize_chain(llm, chain_type="stuff")
     with st.spinner('DemoGPT is working on it. It might take 5-10 seconds...'):
         return chain.run(google_doc)
