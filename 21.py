ff76f2e8c3eec8f46ceb4fd6ff995e9c5bb84757
abdrrman <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-11-05 23:52:41 +0000

    req

--- 21.py
+++ 21.py
@@ -1,3 +1,51 @@
 
+
+import os
 import streamlit as st
-st.title('app-4-21')
+import tempfile
+imports
+
+
+function_defs
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+openai_api_key = st.sidebar.text_input(
+    "OpenAI API Key",
+    placeholder="sk-...",
+    value=os.getenv("OPENAI_API_KEY", ""),
+    type="password",
+)
+
+
+st.sidebar.markdown("""# How to use
+
+1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) above🔑
+
+2. Provide a use case for the real estate application.
+
+3. Wait for the AI to generate a list of cutting-edge use cases based on your provided use case.
+
+4. Review the list of cutting-edge use cases displayed on the screen.""")
+
+
+st.sidebar.markdown("# About")
+st.sidebar.markdown("Real Estate Magic! is an innovative application that leverages advanced technology to provide cutting-edge use cases for the real estate industry. With Real Estate Magic!, users can access real-time property data, analyze market trends, and make informed decisions. Whether you're a buyer, seller, or investor, this app will revolutionize the way you navigate the real estate market.")
+
+with st.form(key="form"):
+    st.title('Real Estate Magic!')
+    use_case = st.text_input("Enter a use case")
+
+    submit_button = st.form_submit_button(label='Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submit_button:
+
+        remaining_code
+        if cutting_edge_use_cases is not None and len(cutting_edge_use_cases) > 0:
+            st.header("Cutting-Edge Use Cases")
+            for use_case in cutting_edge_use_cases:
+                st.subheader(use_case)
